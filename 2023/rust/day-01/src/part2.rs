2913861200ba4a6e0fadf6926c27b3728ba9b841
VV0JC13CH <wojciech@bitstudio.dev>
2023-12-02 16:05:25 +0100

    day1-part2

--- 2023/rust/day-01/src/part2.rs
+++ 2023/rust/day-01/src/part2.rs
@@ -1,10 +1,38 @@
 use crate::custom_error::AocError;
 
 #[tracing::instrument]
-pub fn process(
-    _input: &str,
-) -> miette::Result<String, AocError> {
-    todo!("day 01 - part 1");
+pub fn process(input: &str) -> miette::Result<String, AocError> {
+    let output = input
+        .lines()
+        .map(|line| {
+            let line = line
+                // It can't be done by 
+                // .replace("one", "1")
+                // because of overlapping numbers like
+                // "fivezg8xxttwoneg"  <= "twone"
+                // it can overlapp both from left and right
+                .replace("one", "one1one")
+                .replace("two", "two2two")
+                .replace("three", "three3three")
+                .replace("four", "four4four")
+                .replace("five", "five5five")
+                .replace("six", "six6six")
+                .replace("seven", "seven7seven")
+                .replace("eight", "eight8eight")
+                .replace("nine", "nine9nine")
+            let mut it = line.chars().filter_map(|character| character.to_digit(10));
+            let first = it.next().expect("should be a number");
+
+            match it.last() {
+                Some(num) => format!("{first}{num}"),
+                None => format!("{first}{first}"),
+            }
+            .parse::<u32>()
+            .expect("should be a valid number")
+        })
+        .sum::<u32>();
+
+    Ok(output.to_string())
 }
 
 #[cfg(test)]
@@ -13,9 +41,14 @@ mod tests {
 
     #[test]
     fn test_process() -> miette::Result<()> {
-        todo!("haven't built test yet");
-        let input = "";
-        assert_eq!("", process(input)?);
+        let input = "two1nine
+eightwothree
+abcone2threexyz
+xtwone3four
+4nineeightseven2
+zoneight234
+7pqrstsixteen";
+        assert_eq!("281", process(input)?);
         Ok(())
     }
 }
